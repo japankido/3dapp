38c8947f14fb6ae3ff7d1c705cdfc74d41192359
baidiwangBU <114991957+japankido@users.noreply.github.com>
2023-05-11 17:12:20 +0800

    upload drink page

---
+++ assignment/public/js/script.js
@@ -0,0 +1,11 @@
+$('#roses').click(function () {
+  $('#model_inline').attr("url", "../x3d/roses.x3d");
+});
+
+$('#oasis').click(function () {
+  $('#model_inline').attr("url", "../x3d/oasis.x3d");
+});
+
+$('#monster_energy').click(function () {
+  $('#model_inline').attr("url", "../x3d/monster-energy.x3d");
+});
